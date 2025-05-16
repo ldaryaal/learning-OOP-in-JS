fcadab97cd7a288b96d384376b70f8c52f639a2b
Darya <zahrakhalafy00@gmail.com>
2025-05-16 18:31:07 +0200

    inherit in classes

--- appClass.js
+++ appClass.js
@@ -35,3 +35,18 @@ let person1 = new Client("darya", 2000);
 let person2 = new Client("amin", 200);
 
 console.log(person1);
+
+
+//class business
+class Business extends Client {
+    constructor(name, balance, phone, category ){
+        //super func will get the arguments from Client 
+        super(name, balance)
+        this.phone = phone
+        this.category = category
+    }
+}
+
+const business1 = new Business("Amin", 30000, 3312770000,'Engineer')
+console.log(business1)
+
